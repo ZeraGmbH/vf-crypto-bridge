29428adc70561991f9442743ade8a64164d48c27
Samuel Stirtzel <s.stirtzel@googlemail.com>
2018-09-21 08:59:07 +0200

    Added COPYING.SSL-SSLeay
    The vcb_opensslsignaturehandler.cpp implementation is licensed under the SSL/SSLeay license
    
    Signed-off-by: Samuel Stirtzel <s.stirtzel@googlemail.com>

--- vcb_opensslsignaturehandler.cpp
+++ vcb_opensslsignaturehandler.cpp
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2008-2018 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2018 Zera GmbH
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file COPYING.SSL-SSLeay in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
 #include "vcb_opensslsignaturehandler.h"
 
 #include <QDebug>
